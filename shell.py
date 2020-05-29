3b8a7c69b857b72f82e1a813364322f98fe6c9ca
Bao Nguyen <btn6364@rit.edu>
2020-05-29 17:11:17 -0400

    Split between reading files and interactive shell

--- shell.py
+++ shell.py
@@ -1,13 +1,15 @@
+import sys
 from src.interpreter import Interpreter
+from src.tokenizer import Tokenizer
 
-def main():
+def shell():
     """
-    Interactive shell
-    """
+        Interactive shell
+        """
     while True:
         try:
             text = input('Pylang> ')
-        except EOFError: #Ctrl + D
+        except EOFError:  # Ctrl + D
             break
         if not text:
             continue
@@ -15,5 +17,17 @@ def main():
         result = interpreter.interpret()
         print(result)
 
+def read_file(filename):
+    with open(filename) as file:
+        text = file.read()
+        interpreter = Interpreter(text)
+        result = interpreter.interpret()
+        print(interpreter.GLOBAL_VARS)
+
+
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        read_file(filename)
+    else:
+        shell()

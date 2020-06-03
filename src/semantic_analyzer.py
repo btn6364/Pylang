e6d34f8c9d86e40a154002f9c6f04edad3589563
Bao Nguyen <btn6364@rit.edu>
2020-06-03 16:56:43 -0400

    Seperate semantic analysis logic

---
+++ src/semantic_analyzer.py
@@ -0,0 +1,60 @@
+from src.node_visitor import NodeVisitor
+from src.symbol import VarSymbol
+from src.symbol import SymbolTable
+"""
+Semantic analyzer
+"""
+class SemanticAnalyzer(NodeVisitor):
+    def __init__(self):
+        self.symbol_table = SymbolTable()
+
+    def visit_Block(self, node):
+        for declaration in node.declarations:
+            self.visit(declaration)
+        self.visit(node.compound_statement)
+
+    def visit_Program(self, node):
+        self.visit(node.block)
+
+    def visit_BinOp(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_Num(self, node):
+        pass
+
+    def visit_UnaryOp(self, node):
+        self.visit(node.expr)
+
+    def visit_Compound(self, node):
+        for child in node.children:
+            self.visit(child)
+
+    def visit_NoOp(self, node):
+        pass
+
+    def visit_VariableDeclaration(self, node):
+        type_name = node.type_node.value
+        type_symbol = self.symbol_table.lookup(type_name)
+
+        var_name = node.var_node.value
+        var_symbol = VarSymbol(var_name, type_symbol)
+        self.symbol_table.store(var_symbol)
+
+    def visit_Assign(self, node):
+        var_name = node.left.value
+        var_symbol = self.symbol_table.lookup(var_name)
+        if var_symbol is None: #variable is not yet defined
+            raise NameError(f"{repr(var_name)} is not defined")
+        self.visit(node.right)
+        self.visit(node.left)
+
+    def visit_Var(self, node):
+        var_name = node.value
+        var_symbol = self.symbol_table.lookup(var_name)
+
+        if var_symbol is None:
+            raise NameError(f"{repr(var_name)} is not defined")
+
+    def visit_ProcedureDeclaration(self, node):
+        pass
--- src/symbol.py
+++ src/symbol.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 from src.token_type import INTEGER, REAL
-from src.node_visitor import NodeVisitor
+
 """
 Represents a symbol
 """
@@ -53,7 +53,9 @@ class SymbolTable:
     def __str__(self):
         header = "Symbol table contents:"
         out = ["\n", header, "_" * len(header)]
-        extension = [("%7s: %r") % (key, value) for key, value in self.symbols.items()]
+        extension = []
+        for key, value in self.symbols.items():
+            extension.append(("%7s: %r") % (key, value))
         out.extend(extension)
         out.append("\n")
         return "\n".join(out)
@@ -63,67 +65,12 @@ class SymbolTable:
 
     #store a symbol
     def store(self, symbol):
-        print(f"Define: {symbol}")
+        print(f"Defining: {symbol}")
         self.symbols[symbol.name] = symbol
 
     #look up a symbol
     def lookup(self, name):
-        print(f"Lookup: {name}")
+        print(f"Looking up: {name}")
         return self.symbols.get(name) #either a Symbol or None
 
-"""
-Building symbol table
-"""
-class SymbolTableBuilder(NodeVisitor):
-    def __init__(self):
-        self.symbol_table = SymbolTable()
-
-    def visit_Block(self, node):
-        for declaration in node.declarations:
-            self.visit(declaration)
-        self.visit(node.compound_statement)
-
-    def visit_Program(self, node):
-        self.visit(node.block)
-
-    def visit_BinOp(self, node):
-        self.visit(node.left)
-        self.visit(node.right)
-
-    def visit_Num(self, node):
-        pass
-
-    def visit_UnaryOp(self, node):
-        self.visit(node.expr)
-
-    def visit_Compound(self, node):
-        for child in node.children:
-            self.visit(child)
-
-    def visit_NoOp(self, node):
-        pass
-
-    def visit_VariableDeclaration(self, node):
-        type_name = node.type_node.value
-        type_symbol = self.symbol_table.lookup(type_name)
-        var_name = node.var_node.value
-        var_symbol = VarSymbol(var_name, type_symbol)
-        self.symbol_table.store(var_symbol)
-
-    def visit_Assign(self, node):
-        var_name = node.left.value
-        var_symbol = self.symbol_table.lookup(var_name)
-        if var_symbol is None: #variable is not yet defined
-            raise NameError(f"{repr(var_name)} is not defined")
-        self.visit(node.right)
-
-    def visit_Var(self, node):
-        var_name = node.value
-        var_symbol = self.symbol_table.lookup(var_name)
-
-        if var_symbol is None:
-            raise NameError(f"{repr(var_name)} is not defined")
-
-    def visit_ProcedureDeclaration(self, node):
-        pass
 
